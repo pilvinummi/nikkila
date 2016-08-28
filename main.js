ad2b7fcc83f33bf108ca53480c1472fa1e3182e4
pilvinummi <pilkku@gmail.com>
2016-08-28 16:27:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,38 @@ function init() {
   	}
   };
   
-L.geoJson(geojsonFeature).addTo(map);
+  
+function onEachFeature(feature, layer) {
+	var popupContent = "<p>I started out as a GeoJSON " +
+			feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+	if (feature.properties && feature.properties.popupContent) {
+		popupContent += feature.properties.popupContent;
+	}
+
+	layer.bindPopup(popupContent);
+}
+
+L.geoJson(geojsonFeature, {
+	style: function (feature) {
+		return feature.properties && feature.properties.style;
+	},
+	onEachFeature: onEachFeature,
+	pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, {
+			radius: 8,
+			fillColor: "#ff7800",
+			color: "#000",
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.8
+		});
+	}
+}).addTo(map);  
+
+
+  
+//L.geoJson(geojsonFeature).addTo(map);
 
 //var myLayer = L.geoJson().addTo(map);
 //myLayer.addData(geojsonFeature);
