f831025b70560cea9022e3e19ef4cc720c5de002
pilvinummi <pilkku@gmail.com>
2016-09-23 19:39:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function onEachFeature(feature, layer) {
 
 //var map = L.map('map').setView([32.71, -85.59], 10);
 //var layer = L.esri.basemapLayer('Topographic').addTo(map);
-var geojsonLayer = new L.GeoJSON.AJAX('instagram.geojson', {onEachFeature:popUp}, {style:geojson});
+var geojsonLayer = new L.GeoJSON.AJAX('instagramkoe.geojson', {onEachFeature:popUp}, {style:geojson});
 var myStyle = {"color": "#ff7800", "weight": 4, "opacity": 0.65};
 geojsonLayer.addTo(map);
 
